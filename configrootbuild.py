566d4b3c59b92df12ff19829d7fa94e0ea45e51b
Florine de Geus <florine.de.geus@cern.ch>
2025-03-12 11:18:02 +0100

    Set clang as compiler

--- configrootbuild.py
+++ configrootbuild.py
@@ -100,7 +100,7 @@ if __name__ == "__main__":
         )
     )
 
-    cmake_string = f"cmake -G Ninja -DCMAKE_BUILD_TYPE={args.build_type} -DCMAKE_INSTALL_PREFIX={args.install_dir} {build_opt_string} -Dtesting={args.testing} {args.source_dir}"
+    cmake_string = f"cmake -G Ninja -DCMAKE_BUILD_TYPE={args.build_type} -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_INSTALL_PREFIX={args.install_dir} {build_opt_string} -Dtesting={args.testing} {args.source_dir}"
 
     print(f'Configuring CMake in "{args.build_dir}"')
     print(cmake_string)
